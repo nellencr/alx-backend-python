7bff9d95eb19e68ef4a99814b8d1c2a5bf06d6ff
Nellencr <nellencr@gmail.com>
2023-07-06 13:09:43 +0200

    add solutions

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""Define and annotate the following variables with the specified values"""
+'''Task 4's module.
+'''
 
 
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str = 'Holberton
+school: str = 'Holberton'
